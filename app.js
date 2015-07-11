7c16902be8190a450518c9918c71ceb9a1d1b192
Sebastian Sandberg <Bro@Marcuss-MacBook-Pro-2.local>
2015-07-11 11:49:20 -0700

    Added angular functionality

--- app.js
+++ app.js
@@ -1,30 +1,71 @@
-angular.module('myApp', ['ngRoute'])
+var app = angular.module('myApp', ['ngRoute']);
 
-.controller('MainCtrl', ['$scope', "$location", function($scope, $location) {
+app.service('postService', function(){
+  var postList = [
+        {'body':'sdsa',
+      'date': '2014-21-21'},
+
+        {'body':'asdas',
+          'date': '2014-21-21'}];
+
+  var addPost = function(newPost){
+    postList.push(newPost);
+  };
+
+  var getPosts = function(){
+    return postList;  
+  };
+
+  return{
+    addPost: addPost,
+    getPosts: getPosts
+  }
+
+});
+
+app.controller('MainCtrl', ['$scope', "$location", function($scope, $location) {
 
     // $location.path( "/about" );
     $scope.about = "about.html";
     $scope.blog = "blog.html";
     $scope.submitForm = "form.html"
 
-}])
-.controller('AboutCtrl', ['$scope', function($scope) {
+}]);
+app.controller('AboutCtrl', ['$scope', function($scope) {
 
-}])
-.controller('BlogCtrl', ['$scope', function($scope) {
+}]);
 
-}])
-.controller('FormCtrl', ['$scope', function($scope) {
+app.controller('BlogCtrl', ['$scope', 'postService', function($scope, postService) {
+      $scope.posts = postService.getPosts();
+      // [{
+      //   post: [{
+      //     body: "Some text"
+      //   }]
+      // }];
+}]);
+
+
+
+app.controller('FormCtrl', ['$scope', function($scope, postService) {
 	 $scope.master = {};
+   $scope.posts = [];
+   //$scope.postText = 'CampusHero';
+   $scope.post = {};
+      //$scope.update = function(user) {
+        //$scope.master = angular.copy(user);
+      //};
 
-      $scope.update = function(user) {
-        $scope.master = angular.copy(user);
+      $scope.submit = function(posts){
+        
+          $scope.posts.push(this.post);
+        //  $scope.postText = ""; 
+        
       };
 
-      $scope.reset = function() {
-        $scope.user = angular.copy($scope.master);
-      };
+      //$scope.reset = function() {
+     //   $scope.user = angular.copy($scope.master);
+      //};
 
-      $scope.reset();
+      //$scope.reset();
 
-}])
+}]);
