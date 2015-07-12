7bc2893d381fc87b83f9cf099c34d954c704c51c
Allxie <allxiecleary@gmail.com>
2015-07-12 10:16:12 -0700

    added dependenceis. Moved stuff around in head.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-var app = angular.module('myApp', ['ngRoute']);
-
-
+var app = angular.module('myApp', ['chart.js']);
 
 
 app.service('postService', function(){
@@ -92,32 +90,26 @@ app.controller('BlogCtrl', ['$scope', 'postService', function($scope, postServic
 
 app.controller('StatsCtrl', ['$scope', function($scope){
 
-// 	var myBarChart = new Chart(ctx).Bar(data, options);
+	  // $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+	  // $scope.series = ['Series A', 'Series B'];
 
-// 	var data = {
-//     labels: ["January", "February", "March", "April", "May", "June", "July"],
-//     datasets: [
-//         {
-//             label: "My First dataset",
-//             fillColor: "rgba(220,220,220,0.5)",
-//             strokeColor: "rgba(220,220,220,0.8)",
-//             highlightFill: "rgba(220,220,220,0.75)",
-//             highlightStroke: "rgba(220,220,220,1)",
-//             data: [65, 59, 80, 81, 56, 55, 40]
-//         },
-//         {
-//             label: "My Second dataset",
-//             fillColor: "rgba(151,187,205,0.5)",
-//             strokeColor: "rgba(151,187,205,0.8)",
-//             highlightFill: "rgba(151,187,205,0.75)",
-//             highlightStroke: "rgba(151,187,205,1)",
-//             data: [28, 48, 40, 19, 86, 27, 90]
-//         }
-//     ]
-// };
+	  // $scope.data = [
+	  //   [65, 59, 80, 81, 56, 55, 40],
+	  //   [28, 48, 40, 19, 86, 27, 90]
+	  // ];
+
 
 
 }]);
+app.controller("BarCtrl", function ($scope) {
+  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+});
 
 app.controller('FormCtrl', ['$scope', 'postService', function($scope, postService) {
       var tags = [];
